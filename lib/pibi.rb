eaf58bd304d9553fb13f9c5bd4e7a86d7bfc9382
Ahmad Amireh <ahmad.amireh@gmail.com>
2013-08-30 11:16:24 +0300

    Updated license body

--- lib/pibi.rb
+++ lib/pibi.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 Ahmad Amireh <ahmad@amireh.net>
+# Copyright (c) 2013 Algol Labs, LLC. <dev@algollabs.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
--- lib/pibi/version.rb
+++ lib/pibi/version.rb
@@ -1,5 +1,4 @@
-#
-# Copyright (c) 2013 Instructure, Inc.
+# Copyright (c) 2013 Algol Labs, LLC. <dev@algollabs.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
